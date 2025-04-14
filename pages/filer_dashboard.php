98a7e8e9dbc594a072b91f31031f1fd313c877d0
John Neil <johnmontiano8@gmail.com>
2025-04-14 15:48:22 +0800

    temporary design

--- pages/filer_dashboard.php
+++ pages/filer_dashboard.php
@@ -4,13 +4,33 @@
     <div id="account_dashboard" class="account_dashboard" style="display: block;">
         <div class="card shadow mb-4">
             <div class="card-header py-3.5 pt-4">
-                <div class="col-md-6">
-                    <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Ongoing Trouble Report Request</a>
-                    <a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Finished Trouble Report Request</a>
+                <div class="col-md-12">
+                    <a href="#" class="btn btn-primary active" role="button" aria-pressed="true">Ongoing Trouble Report Request</a>
+                    <a href="#" class="btn btn-primary active" role="button" aria-pressed="true">Finished Trouble Report Request</a>
                 </div>
 
-                <div>
-                    
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class=" table table-bordered table-striped" id="dataTable" width="100%" cellspacing="0">
+                            <thead class="bg-primary text-white">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>---</th>
+                                    <th>---</th>
+                                    <th>---</th>
+                                    <th style="width: 170px;">Actions</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tbody>
+                        </table>
+                    </div>        
                 </div>
             </div>
         </div>
