8e78936d601d1c609bc5ec256f99f6e832449b72
John Neil <johnmontiano8@gmail.com>
2025-04-14 15:01:53 +0800

    Refactor header.php for improved readability; adjust PHP include formatting and clean up commented sections

--- include/header.php
+++ include/header.php
@@ -1,9 +1,11 @@
-<?php include '../include/connect.php'; ?>
+<?php 
+  include '../include/connect.php'; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,18 +17,12 @@
   <link rel="icon" type="image/png" sizes="32x32" href="../assets/img/logo.png">
 
   <?php include 'link.php'; ?>
-
 </head>
 
 <body id="page-top">
-
-  <!-- Page Wrapper -->
   <div id="wrapper">
-
-    <!-- Sidebar -->
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-      <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="sidebar-brand-icon">
         <img src="../assets/img/logo.png" alt="" class="img-fluid" style="width: 45px;">
@@ -34,20 +30,22 @@
         <div class="sidebar-brand-text mx-2">QC-TRS</div>
       </a>
 
-      <!-- Divider -->
       <hr class="sidebar-divider my-0">
 
-      <!-- Nav Item - Dashboard -->
       <li class="nav-item">
+        <a class="nav-link" href="../pages/account.php">
+        <i class="fa fa-user" aria-hidden="true"></i>
+          <span>Manage Account</span></a>
+      </li>
+      
+      <!-- <li class="nav-item">
         <a class="nav-link" href="../pages/dashboard.php">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
-      </li>
+      </li> -->
 
-      <!-- Divider -->
       <!-- <hr class="sidebar-divider"> -->
 
-      <!-- Heading -->
       <!-- <div class="sidebar-heading">
         Components
       </div> -->
@@ -58,44 +56,31 @@
           <span>Link</span></a>
       </li> -->
 
-      <!-- Divider -->
       <!-- <hr class="sidebar-divider d-none d-md-block"> -->
 
-      <!-- Sidebar Toggler (Sidebar) -->
       <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
       </div>
-
     </ul>
-    <!-- End of Sidebar -->
 
-    <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
-
-      <!-- Main Content -->
       <div id="content">
-
-        <!-- Topbar -->
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-          <!-- Sidebar Toggle (Topbar) -->
           <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
           </button>
 
-          <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
-
             <!-- <div class="topbar-divider d-none d-sm-block"></div> -->
 
-            <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
                 <img class="img-profile rounded-circle" src="../assets/img/undraw_profile.svg" alt="Picture">
               </a>
-              <!-- Dropdown - User Information -->
+
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="userDropdown">
                 <!-- <a class="dropdown-item" href="#">
@@ -117,8 +102,6 @@
                 </a>
               </div>
             </li>
-
           </ul>
-
         </nav>
-        <!-- End of Topbar -->
+      
