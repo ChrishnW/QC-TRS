c56ab83be9236d06c800544da3c19d69f328c2ca
John Neil <johnmontiano8@gmail.com>
2025-04-15 07:57:41 +0800

    temporary UI

--- pages/filer_dashboard.php
+++ pages/filer_dashboard.php
@@ -3,14 +3,57 @@
 <div class="container-fluid">
     <div id="account_dashboard" class="account_dashboard" style="display: block;">
         <div class="card shadow mb-4">
-            <div class="card-header py-3.5 pt-4">
-                <div class="col-md-12">
+            <div class="card-header ">
+                <div class="cold-md-6 float-left">
                     <a href="#" class="btn btn-primary active" role="button" aria-pressed="true">Ongoing Trouble Report Request</a>
                     <a href="#" class="btn btn-primary active" role="button" aria-pressed="true">Finished Trouble Report Request</a>
-                </div>              
+                </div>  
             </div>
 
+            <div>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="container-fluid">
+    <div id="account_dashboard" class="account_dashboard" style="display: block;">
+        <div class="card shadow mb-4">
             <div class="card-body">
+            <h2 class="float-left">Ongoing Trouble Report</h2>   
+                <div class="table-responsive">
+                    <table class=" table table-bordered table-striped" id="dataTable" width="100%" cellspacing="0">
+                        <thead class="bg-primary text-white">
+                            <tr>
+                                <th>ID</th>
+                                <th>---</th>
+                                <th>---</th>
+                                <th>---</th>
+                                <th style="width: 170px;">Actions</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tbody>
+                    </table>
+                </div>        
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div id="account_dashboard" class="account_dashboard" style="display: block;">
+        <div class="card shadow mb-4">
+            <div class="card-body">
+                <h2 class="float-left">Finished Trouble Report</h2>
                 <div class="table-responsive">
                     <table class=" table table-bordered table-striped" id="dataTable" width="100%" cellspacing="0">
                         <thead class="bg-primary text-white">
