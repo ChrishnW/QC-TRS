275a0258ad361863cc4e4cc1a35f2a2c0d6940ec
John Neil <johnmontiano8@gmail.com>
2025-04-15 08:53:26 +0800

    Refactor: Update page titles for admin, approver, auditor, filer, and maker headers

--- pages/filer_request.php
+++ pages/filer_request.php
@@ -10,108 +10,99 @@
 
             <form>
                 <div class="modal-body">
-                    <div>
-                        <div class="card mb-4">
-                            <div class="card-body">
+                    <div class="card-body">
 
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="">Date <span style="color: red;">*</span></label><br>
-                                        <input type="date" class="form-control" placeholder="mm/dd/yyyy" required>
-                                    </div>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="">Date <span style="color: red;">*</span></label><br>
+                                <input type="date" class="form-control" placeholder="mm/dd/yyyy" required>
+                            </div>
 
-                                    <div class="col-md-6">
-                                        <label for="">Department <span style="color: red;">*</span></label><br>
-                                        <input type="text" class="form-control" placeholder="Department Name" required>
-                                    </div>
-                                </div>
-
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="">Model <span style="color: red;">*</span></label><br>
-                                        <input type="text" class="form-control" placeholder="Machine Name" required>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <label for="">Lot Number <span style="color: red;">*</span></label>
-                                        <input type="text" class="form-control" placeholder="Lot Number" required>
-                                    </div>
-                                </div>
-
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="">Serial Number <span style="color: red;">*</span></label><br>
-                                        <input type="text" class="form-control" placeholder="Serial Number" required>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <label for="">Temp # <span style="color: red;">*</span></label><br>
-                                        <input type="text" class="form-control" placeholder="Temp Number" required>
-                                    </div>
-                                </div>
-
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="">Quantity <span style="color: red;">*</span></label><br>
-                                        <input type="number" class="form-control" placeholder="Quantity" required>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <label for="">Due Date <span style="color: red;">*</span></label><br>
-                                        <input type="date" class="form-control" placeholder="mm/dd/yyyy" required>
-                                    </div>
-                                </div>
-
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="">Image (Good) <span style="color: red;">*</span></label><br>
-                                        <input type="file" class="form-control" placeholder="Image Good" required>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <label for="">Image (Not Good) <span style="color: red;">*</span></label><br>
-                                        <input type="file" class="form-control" placeholder="Image Not Good" required>
-                                    </div>
-                                </div>
-
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="">Trouble Origin (100%) <span style="color: red;">*</span></label><br>
-                                        <input type="text" class="form-control" placeholder="Trouble Origin (100%)" required>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <label for="">Checked by (200%) <span style="color: red;">*</span></label><br>
-                                        <input type="text" class="form-control" placeholder="Trouble Origin (200%)" required>
-                                    </div>
-                                </div>
-                                
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="">Found by (QC) <span style="color: red;">*</span></label><br>
-                                        <input type="text" class="form-control" placeholder="Trouble Finder (QC)" required>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                        <label for="">Found by (AI) <span style="color: red;">*</span></label><br>
-                                        <input type="text" class="form-control" placeholder="Trouble Finder (AI)" required>
-                                    </div>
-                                </div>
-
-                                <div class="row mb-3">
-                                    <div class="col-md-6">
-                                        <label for="">Findings <span style="color: red;">*</span></label><br>
-                                        <textarea class="form-control" rows="5" placeholder="Problem Description" required></textarea>
-                                    </div>
-
-                                    <div class="col-md-6">
-                                      
-                                    </div>
-                                </div>
+                            <div class="col-md-6">
+                                <label for="">Department <span style="color: red;">*</span></label><br>
+                                <input type="text" class="form-control" placeholder="Department Name" required>
                             </div>
                         </div>
-                    </div>
-                
+
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="">Model <span style="color: red;">*</span></label><br>
+                                <input type="text" class="form-control" placeholder="Machine Name" required>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="">Lot Number <span style="color: red;">*</span></label>
+                                <input type="text" class="form-control" placeholder="Lot Number" required>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="">Serial Number <span style="color: red;">*</span></label><br>
+                                <input type="text" class="form-control" placeholder="Serial Number" required>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="">Temp # <span style="color: red;">*</span></label><br>
+                                <input type="text" class="form-control" placeholder="Temp Number" required>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="">Quantity <span style="color: red;">*</span></label><br>
+                                <input type="number" class="form-control" placeholder="Quantity" required>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="">Due Date <span style="color: red;">*</span></label><br>
+                                <input type="date" class="form-control" placeholder="mm/dd/yyyy" required>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="">Image (Good) <span style="color: red;">*</span></label><br>
+                                <input type="file" class="form-control" placeholder="Image Good" required>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="">Image (Not Good) <span style="color: red;">*</span></label><br>
+                                <input type="file" class="form-control" placeholder="Image Not Good" required>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="">Trouble Origin (100%) <span style="color: red;">*</span></label><br>
+                                <input type="text" class="form-control" placeholder="Trouble Origin (100%)" required>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="">Checked by (200%) <span style="color: red;">*</span></label><br>
+                                <input type="text" class="form-control" placeholder="Trouble Origin (200%)" required>
+                            </div>
+                        </div>
+                        
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="">Found by (QC) <span style="color: red;">*</span></label><br>
+                                <input type="text" class="form-control" placeholder="Trouble Finder (QC)" required>
+                            </div>
+
+                            <div class="col-md-6">
+                                <label for="">Found by (AI) <span style="color: red;">*</span></label><br>
+                                <input type="text" class="form-control" placeholder="Trouble Finder (AI)" required>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <div class="col-md-12">
+                                <label for="">Findings <span style="color: red;">*</span></label><br>
+                                <textarea class="form-control" rows="5" placeholder="Problem Description" required></textarea>
+                            </div>
+                        </div>
+                    </div>                
                 </div> 
 
                 <div class="modal-footer">
