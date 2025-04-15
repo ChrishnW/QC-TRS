59ce2907405b9d51e23cab137a8a50314086759f
John Neil <johnmontiano8@gmail.com>
2025-04-16 07:48:24 +0800

    Refactor: Improve layout and button functionality for ongoing and finished trouble reports

--- pages/maker_dashboard.php
+++ pages/maker_dashboard.php
@@ -1,21 +1,20 @@
 <?php include '../include/header_maker.php'; ?>
 
+<!-- Ongoing Trouble Report -->
 <div class="container-fluid" id="ongoing_trouble_report" style="display: block;">
-    <div class="maker_dashboard">
+    <div class="filer_dashboard">
         <div class="card shadow mb-4">
+            <div class="card-header py-3.5 pt-4">
+                <h2 class="float-left">Ongoing Trouble Report</h2>
 
-            <div class="cold-md-6 float-left m-3">
-                <a href="#" class="btn btn-primary active" onclick="display_ongoing()" role="button" aria-pressed="true">Ongoing Trouble Report Request</a>
-                <a href="#" class="btn btn-primary active" onclick="display_finished()" role="button" aria-pressed="true">Finished Trouble Report Request</a>
-            </div> 
-
-            <div class="card-header py-3.5 pt-1">
-                <h2 class="float-left">Ongoing Trouble Report</h2>            
+                <div class="btn-group float-right" role="group" aria-label="Switch Buttons">
+                    <button id="ongoingBtn" type="button" class="btn btn-outline-primary active" onclick="display_ongoing()">Ongoing</button>
+                    <button id="finishedBtn" type="button" class="btn btn-outline-primary" onclick="display_finished()">Finished</button>
+                </div>
             </div>
-
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class=" table table-bordered table-striped" id="ongoing_dataTable" width="100%" cellspacing="0">
+                    <table class="table table-bordered table-striped" id="ongoing_dataTable" width="100%" cellspacing="0">
                         <thead class="bg-primary text-white">
                             <tr>
                                 <th>ID</th>
@@ -25,37 +24,37 @@
                                 <th style="width: 170px;">Actions</th>
                             </tr>
                         </thead>
-
                         <tbody>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
                         </tbody>
                     </table>
-                </div>        
+                </div>
             </div>
         </div>
     </div>
 </div>
 
+<!-- Finished Trouble Report -->
 <div class="container-fluid" id="finished_trouble_report" style="display: none;">
-    <div class="maker_dashboard">
+    <div class="filer_dashboard">
         <div class="card shadow mb-4">
-
-            <div class="cold-md-6 float-left m-3">
-                <a href="#" class="btn btn-primary active" onclick="display_ongoing()" role="button" aria-pressed="true">Ongoing Trouble Report Request</a>
-                <a href="#" class="btn btn-primary active" onclick="display_finished()" role="button" aria-pressed="true">Finished Trouble Report Request</a>
-            </div> 
-
-            <div class="card-header py-3.5 pt-1">
+            <div class="card-header py-3.5 pt-4">
                 <h2 class="float-left">Finished Trouble Report</h2>
-            </div>
 
+                <div class="btn-group float-right" role="group" aria-label="Switch Buttons">
+                    <button id="ongoingBtn" type="button" class="btn btn-outline-primary" onclick="display_ongoing()">Ongoing</button>
+                    <button id="finishedBtn" type="button" class="btn btn-outline-primary active" onclick="display_finished()">Finished</button>
+                </div>
+            </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class=" table table-bordered table-striped" id="finished_dataTable" width="100%" cellspacing="0">
+                    <table class="table table-bordered table-striped" id="finished_dataTable" width="100%" cellspacing="0">
                         <thead class="bg-primary text-white">
                             <tr>
                                 <th>ID</th>
@@ -65,16 +64,17 @@
                                 <th style="width: 170px;">Actions</th>
                             </tr>
                         </thead>
-
                         <tbody>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
                         </tbody>
                     </table>
-                </div>        
+                </div>
             </div>
         </div>
     </div>
@@ -83,7 +83,6 @@
 <?php include '../include/footer.php'; ?>
 
 <script>
-
     $(document).ready(function() {
         $('#ongoing_dataTable').DataTable();
         $('#finished_dataTable').DataTable();
@@ -92,11 +91,14 @@
     function display_ongoing() {
         document.getElementById("ongoing_trouble_report").style.display = "block";
         document.getElementById("finished_trouble_report").style.display = "none";
-    };
+        document.getElementById("ongoingBtn").classList.add('active');
+        document.getElementById("finishedBtn").classList.remove('active');
+    }
 
     function display_finished() {
         document.getElementById("ongoing_trouble_report").style.display = "none";
         document.getElementById("finished_trouble_report").style.display = "block";
-    };
-
+        document.getElementById("finishedBtn").classList.add('active');
+        document.getElementById("ongoingBtn").classList.remove('active');
+    }
 </script>
