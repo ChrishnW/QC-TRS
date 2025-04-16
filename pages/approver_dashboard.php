0f41151d8724350233aeda624cd8b492f4d470d6
John Neil <johnmontiano8@gmail.com>
2025-04-16 09:25:36 +0800

    approver dashboard prototype

--- pages/approver_dashboard.php
+++ pages/approver_dashboard.php
@@ -4,18 +4,19 @@
 <div class="container-fluid" id="pending_reports" style="display: block;">   
     <div class="pending_dashboard">
         <div class="card shadow mb-4">
-            <div class="card-body">
-                <h2 class="text-left">Pending Approvals</h2>
-                <p class="text-left">Review and approve trouble report requests.</p> 
-
-                <div class="d-flex justify-content-left pb-2">
+            <div class="card-header py-3.5 pt-4">
+                <h2 class="float-left">Pending Approvals</h2>
+                
+                <div class="btn-group float-right pb-2">
                     <div class="btn-group" role="group" aria-label="Switch Buttons">
                         <button id="display_pending" type="button" class="btn btn-outline-primary active" onclick="display_pending()">Pending Approval</button>
                         <button id="display_approved" type="button" class="btn btn-outline-primary" onclick="display_approved()">Approved Reports</button>
                         <button id="display_rejected" type="button" class="btn btn-outline-primary" onclick="display_rejected()">Rejected Reports</button>
                     </div>
                 </div>
+            </div>
 
+            <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-bordered table-striped" id="pending_dataTable" width="100%" cellspacing="0">
                         <thead class="bg-primary text-white">
@@ -41,18 +42,19 @@
 <div class="container-fluid" id="approved_reports" style="display: none;">   
     <div class="approved_dashboard">
         <div class="card shadow mb-4">
-            <div class="card-body">
-                <h2 class="text-left">Approved Reports</h2>
-                <p class="text-left">View all approved trouble report requests.</p> 
-
-                <div class="d-flex justify-content-left pb-2">
+            <div class="card-header py-3.5 pt-4">
+                <h2 class="float-left">Approved Reports</h2>
+               
+                <div class="btn-group float-right pb-2">
                     <div class="btn-group" role="group" aria-label="Switch Buttons">
                         <button id="display_pending" type="button" class="btn btn-outline-primary" onclick="display_pending()">Pending Approval</button>
                         <button id="display_approved" type="button" class="btn btn-outline-primary active" onclick="display_approved()">Approved Reports</button>
                         <button id="display_rejected" type="button" class="btn btn-outline-primary" onclick="display_rejected()">Rejected Reports</button>
                     </div>
                 </div>
-
+            </div>
+            
+            <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-bordered table-striped" id="approved_dataTable" width="100%" cellspacing="0">
                         <thead class="bg-primary text-white">
@@ -78,18 +80,19 @@
 <div class="container-fluid" id="rejected_reports" style="display: none;">   
     <div class="rejected_dashboard">
         <div class="card shadow mb-4">
-            <div class="card-body">
-                <h2 class="text-left">Rejected Reports</h2>
-                <p class="text-left">View all rejected trouble report requests.</p> 
+            <div class="card-header py-3.5 pt-4">
+                <h2 class="float-left">Rejected Reports</h2>
 
-                <div class="d-flex justify-content-left pb-2">
+                <div class="btn-group float-right pb-2">
                     <div class="btn-group" role="group" aria-label="Switch Buttons">
                         <button id="display_pending" type="button" class="btn btn-outline-primary" onclick="display_pending()">Pending Approval</button>
                         <button id="display_approved" type="button" class="btn btn-outline-primary" onclick="display_approved()">Approved Reports</button>
                         <button id="display_rejected" type="button" class="btn btn-outline-primary active" onclick="display_rejected()">Rejected Reports</button>
                     </div>
                 </div>
-
+            </div>
+            
+            <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-bordered table-striped" id="rejected_dataTable" width="100%" cellspacing="0">
                         <thead class="bg-primary text-white">
