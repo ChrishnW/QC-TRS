28083087b35a60f42495c1d24df0ea115957f5fc
John Neil <johnmontiano8@gmail.com>
2025-04-16 08:33:46 +0800

    Refactor: Simplify form inputs and enhance approval section in filer_request.php

--- pages/approver_dashboard.php
+++ pages/approver_dashboard.php
@@ -1,6 +1,18 @@
 <?php include '../include/header_approver.php'; ?>
+
 <div class="container-fluid">   
-    
+    <div class="filer_dashboard">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3.5 pt-4">
+                <h2 class="float-left">Ongoing Trouble Report Request</h2>
+
+                <div class="btn-group float-right" role="group" aria-label="Switch Buttons">
+                    <button id="pendingApproval" type="button" class="btn btn-outline-primary active" onclick="display_ongoing()">Ongoing</button>
+                    <button id="approved" type="button" class="btn btn-outline-primary" onclick="display_finished()">Finished</button>
+                    <button id="finishedBtn" type="button" class="btn btn-outline-primary" onclick="display_finished()">Finished</button>
+                </div>
+            </div>
 
 </div>
+
 <?php include '../include/footer.php'; ?>
--- pages/filer_request.php
+++ pages/filer_request.php
@@ -13,96 +13,134 @@
                     <div class="row mb-3">
                         <div class="col-md-6">
                             <label for="">Date <span style="color: red;">*</span></label><br>
-                            <input type="date" class="form-control" placeholder="mm/dd/yyyy" required>
+                            <input type="date" class="form-control" required>
                         </div>
 
                         <div class="col-md-6">
                             <label for="">Department <span style="color: red;">*</span></label><br>
-                            <input type="text" class="form-control" placeholder="Department Name" required>
+                            <input type="text" class="form-control" required>
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <div class="col-md-6">
                             <label for="">Model <span style="color: red;">*</span></label><br>
-                            <input type="text" class="form-control" placeholder="Machine Name" required>
+                            <input type="text" class="form-control" required>
                         </div>
 
-                        <div class="col-md-6">
-                            <label for="">Lot Number <span style="color: red;">*</span></label>
-                            <input type="text" class="form-control" placeholder="Lot Number" required>
-                        </div>
-                    </div>
-
-                    <div class="row mb-3">
-                        <div class="col-md-6">
-                            <label for="">Serial Number <span style="color: red;">*</span></label><br>
-                            <input type="text" class="form-control" placeholder="Serial Number" required>
-                        </div>  
-
-                        <div class="col-md-6">
-                            <label for="">Temp Number <span style="color: red;">*</span></label><br>
-                            <input type="text" class="form-control" placeholder="Temp Number" required>
-                        </div>
-                  </div>
-
-                    <div class="row mb-3">
                         <div class="col-md-6">
                             <label for="">Quantity <span style="color: red;">*</span></label><br>
                             <input type="number" class="form-control" placeholder="0" required min="0">
-                        </div>                     
+                        </div>   
                     </div>
+
+                    <div class="row mb-3">
+                    <div class="col-md-4">
+                            <label for="">Lot Number <span style="color: red;">*</span></label>
+                            <input type="text" class="form-control" required>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label for="">Serial Number <span style="color: red;">*</span></label><br>
+                            <input type="text" class="form-control" required>
+                        </div>  
+
+                        <div class="col-md-4">
+                            <label for="">Temp Number <span style="color: red;">*</span></label><br>
+                            <input type="text" class="form-control" required>
+                        </div>
+                    </div>
+
+                    <hr class="mt-4">
 
                     <div class="row mb-3">
                         <div class="col-md-12">
                             <label for="">Findings <span style="color: red;">*</span></label><br>
-                            <textarea class="form-control" rows="5" placeholder="Problem Description" required></textarea>
+                            <textarea class="form-control" rows="5" required></textarea>
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <div class="col-md-6">
                             <label for="">Trouble Origin (100%) <span style="color: red;">*</span></label><br>
-                            <input type="text" class="form-control" placeholder="Trouble Origin (100%)" required>
+                            <input type="text" class="form-control" required>
                         </div>
 
                         <div class="col-md-6">
                             <label for="">Checked by (200%) <span style="color: red;">*</span></label><br>
-                            <input type="text" class="form-control" placeholder="Trouble Origin (200%)" required>
+                            <input type="text" class="form-control" required>
                         </div>
                     </div>
                     
                     <div class="row mb-3">
                         <div class="col-md-6">
                             <label for="">Found by (QC) <span style="color: red;">*</span></label><br>
-                            <input type="text" class="form-control" placeholder="Trouble Finder (QC)" required>
+                            <input type="text" class="form-control" required>
                         </div>
 
                         <div class="col-md-6">
                             <label for="">Found by (AI) <span style="color: red;">*</span></label><br>
-                            <input type="text" class="form-control" placeholder="Trouble Finder (AI)" required>
+                            <input type="text" class="form-control" required>
                         </div>
                     </div>                   
 
                     <div class="row mb-3">
                         <div class="col-md-6">
                             <label for="">Image (Good) <span style="color: red;">*</span></label><br>
-                            <input type="file" class="form-control" style="height: auto;" placeholder="Upload Image" required accept=".jpg,.jpeg,.png,.pdf">
+                            <input type="file" class="form-control" style="height: auto;" required accept=".jpg,.jpeg,.png,.pdf">
                         </div>
 
                         <div class="col-md-6">
                             <label for="">Image (Not Good) <span style="color: red;">*</span></label><br>
-                            <input type="file" class="form-control" style="height: auto;" placeholder="Upload Image" required accept=".jpg,.jpeg,.png,.pdf">
+                            <input type="file" class="form-control" style="height: auto;" required accept=".jpg,.jpeg,.png,.pdf">
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <div class="col-md-6">
                             <label for="">Due Date <span style="color: red;">*</span></label><br>
-                            <input type="date" class="form-control" placeholder="mm/dd/yyyy" required>
+                            <input type="date" class="form-control" required>
                         </div>
                     </div>
-                </div>                
+
+                    <hr class="mt-4">
+
+                    <h5>Approval</h5>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="">Leader <span style="color: red;">*</span></label><br>
+                            <select name="status" class="form-control" required >
+                                <option value="<?php echo $status ?>" hidden><?php echo $statusName ?></option>
+                                <option value="1">Acive</option>
+                                <option value="0">Inactive</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="">Department Head <span style="color: red;">*</span></label><br>
+                            <select name="status" class="form-control" required >
+                                <option value="<?php echo $status ?>" hidden><?php echo $statusName ?></option>
+                                <option value="1">Acive</option>
+                                <option value="0">Inactive</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="">Factory Officer <span style="color: red;">*</span></label><br>
+                            <select name="status" class="form-control" required >
+                                <option value="<?php echo $status ?>" hidden><?php echo $statusName ?></option>
+                                <option value="1">Acive</option>
+                                <option value="0">Inactive</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="">COO <span style="color: red;">*</span></label><br>
+                            <select name="status" class="form-control" required >
+                                <option value="<?php echo $status ?>" hidden><?php echo $statusName ?></option>
+                                <option value="1">Acive</option>
+                                <option value="0">Inactive</option>
+                            </select>
+                        </div>
+                    </div>
+
 
                 <div class="modal-footer">
                     <input type="reset" name="reset" value="Cancel" id="cancel_add_breaktime"  class="btn btn-secondary ml-2">
