e92efaef1f58f4ff78b417b0f05b3a3d2a407b95
s3rb0g <raniegobres27@gmail.com>
2025-04-16 14:38:50 +0800

    Refactor ongoing and finished trouble report tables for improved structure and clarity

--- pages/maker_dashboard.php
+++ pages/maker_dashboard.php
@@ -17,21 +17,66 @@
                     <table class="table table-bordered table-striped" id="ongoing_dataTable" width="100%" cellspacing="0">
                         <thead class="bg-primary text-white">
                             <tr>
-                                <th>ID</th>
-                                <th>---</th>
-                                <th>---</th>
-                                <th>---</th>
-                                <th>Actions</th>
+                                <th rowspan="2" class="text-center align-middle">Date</th>
+                                <th rowspan="2" class="text-center align-middle">Model</th>
+                                <th rowspan="2" class="text-center align-middle">Department</th>
+                                <th colspan="4" class="text-center align-middle">Approval Status</th>
+                                <th rowspan="2" class="text-center align-middle">Actions</th>
+                            </tr>
+                            <tr>
+                                <th class="text-center align-middle border-top-0">Line Leader</th>
+                                <th class="text-center align-middle border-top-0">Department Head</th>
+                                <th class="text-center align-middle border-top-0">Factory Officer</th>
+                                <th class="text-center align-middle border-top-0">COO</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                            </tr>
+                            
+                            <?php
+                                $result = mysqli_query($conn, "SELECT * FROM tbl_request INNER JOIN tbl_response ON tbl_request.id=tbl_response.request_id WHERE tbl_request.status=1"); 
+                                if(mysqli_num_rows($result) > 0) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        $request_id = $row['request_id'];
+                                        $response_id = $row['id'];
+
+                                        $date = $row['date'];
+                                        $model = $row['model'];
+                                        $department = $row['dept_status'];
+                                        $line_leader = $row['leader_status'];
+                                        $department_head = $row['dept_head_status'];
+                                        $factory_officer = $row['factory_status'];
+                                        $coo = $row['coo_status'];
+
+                                        $department_status = getApprovalStatus($department);
+                                        $line_leader_status = getApprovalStatus($line_leader);
+                                        $department_head_status = getApprovalStatus($department_head);
+                                        $factory_officer_status = getApprovalStatus($factory_officer);
+                                        $coo_status = getApprovalStatus($coo);
+                            ?>
+
+                                <tr>
+                                    <td><?php echo $date ?></td>
+                                    <td><?php echo $model ?></td>
+                                    <td><?php echo $department_status ?></td>
+                                    <td><?php echo $line_leader_status ?></td>
+                                    <td><?php echo $department_head_status ?></td>
+                                    <td><?php echo $factory_officer_status ?></td>
+                                    <td><?php echo $coo_status ?></td>
+                                    <td>
+                                        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+                                            <input type="hidden" name="request_id" value="<?php echo $request_id; ?>">
+                                            <input type="hidden" name="response_id" value="<?php echo $response_id; ?>">
+                                        
+                                            <input type="submit" name="view_request" class="btn btn-primary" value="View">
+                                        </form>
+                                    </td>
+                                </tr>
+
+                            <?php
+                                    }
+                                }
+                            ?>
+                        
                         </tbody>
                     </table>
                 </div>
@@ -57,21 +102,66 @@
                     <table class="table table-bordered table-striped" id="finished_dataTable" width="100%" cellspacing="0">
                         <thead class="bg-primary text-white">
                             <tr>
-                                <th>ID</th>
-                                <th>---</th>
-                                <th>---</th>
-                                <th>---</th>
-                                <th>Actions</th>
+                                <th rowspan="2" class="text-center align-middle">Date</th>
+                                <th rowspan="2" class="text-center align-middle">Model</th>
+                                <th rowspan="2" class="text-center align-middle">Department</th>
+                                <th colspan="4" class="text-center align-middle">Approval Status</th>
+                                <th rowspan="2" class="text-center align-middle">Actions</th>
+                            </tr>
+                            <tr>
+                                <th class="text-center align-middle border-top-0">Line Leader</th>
+                                <th class="text-center align-middle border-top-0">Department Head</th>
+                                <th class="text-center align-middle border-top-0">Factory Officer</th>
+                                <th class="text-center align-middle border-top-0">COO</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>ang</td>
-                                <td>hirap</td>
-                                <td>labanan</td>
-                                <td>ng</td>
-                                <td>antok</td>
-                            </tr>
+                            
+                            <?php
+                                $result = mysqli_query($conn, "SELECT * FROM tbl_request INNER JOIN tbl_response ON tbl_request.id=tbl_response.request_id WHERE tbl_request.status=1"); 
+                                if(mysqli_num_rows($result) > 0) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        $request_id = $row['request_id'];
+                                        $response_id = $row['id'];
+
+                                        $date = $row['date'];
+                                        $model = $row['model'];
+                                        $department = $row['dept_status'];
+                                        $line_leader = $row['leader_status'];
+                                        $department_head = $row['dept_head_status'];
+                                        $factory_officer = $row['factory_status'];
+                                        $coo = $row['coo_status'];
+
+                                        $department_status = getApprovalStatus($department);
+                                        $line_leader_status = getApprovalStatus($line_leader);
+                                        $department_head_status = getApprovalStatus($department_head);
+                                        $factory_officer_status = getApprovalStatus($factory_officer);
+                                        $coo_status = getApprovalStatus($coo);
+                            ?>
+
+                                <tr>
+                                    <td><?php echo $date ?></td>
+                                    <td><?php echo $model ?></td>
+                                    <td><?php echo $department_status ?></td>
+                                    <td><?php echo $line_leader_status ?></td>
+                                    <td><?php echo $department_head_status ?></td>
+                                    <td><?php echo $factory_officer_status ?></td>
+                                    <td><?php echo $coo_status ?></td>
+                                    <td>
+                                        <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+                                            <input type="hidden" name="request_id" value="<?php echo $request_id; ?>">
+                                            <input type="hidden" name="response_id" value="<?php echo $response_id; ?>">
+                                        
+                                            <input type="submit" name="view_request" class="btn btn-primary" value="View">
+                                        </form>
+                                    </td>
+                                </tr>
+
+                            <?php
+                                    }
+                                }
+                            ?>
+                        
                         </tbody>
                     </table>
                 </div>
