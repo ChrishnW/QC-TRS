8e77dfab06f67bd6d29574ff13f4977c8c7cdd94
s3rb0g <raniegobres27@gmail.com>
2025-04-21 07:37:34 +0800

    Add close button to ongoing trouble report modal for improved user experience

--- pages/maker_dashboard.php
+++ pages/maker_dashboard.php
@@ -192,7 +192,10 @@
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header bg-gradient-primary">
-          <h5 class="modal-title text-white">Ongoing Trouble Report Form</h5>
+            <h5 class="modal-title text-white">Ongoing Trouble Report Form</h5>
+            <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close" onclick="closeView()">
+                <span aria-hidden="true">&times;</span>
+            </button>
         </div>
 
         <div class="modal-body">
