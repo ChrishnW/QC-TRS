5e25ea05ccfc0b4227139194a31468a90777dc53
s3rb0g <raniegobres27@gmail.com>
2025-04-21 07:54:07 +0800

    Update sidebar links to point to respective dashboard pages for admin, approver, auditor, filer, and maker

--- include/header_admin.php
+++ include/header_admin.php
@@ -20,7 +20,7 @@
   <div id="wrapper">
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="admin_dashboard.php">
         <div class="sidebar-brand-icon">
         <img src="../assets/img/logo.png" alt="" class="img-fluid" style="width: 45px;">
         </div>
