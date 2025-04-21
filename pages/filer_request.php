2280378bdb193e2609aafd2f61f707b5732afee6
John Neil <johnmontiano8@gmail.com>
2025-04-21 10:54:18 +0800

    Fix reset button alignment in filer request modal for improved UI consistency

--- pages/filer_request.php
+++ pages/filer_request.php
@@ -277,7 +277,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 <div class="modal-footer">
                     <input type="submit" name="request_submit" value="Submit" class="btn btn-primary">
-                    <input type="reset" value="Reset" class="btn btn-secondary mr-n2">
+                    <input type="reset" value="Reset" class="btn btn-secondary">
                 </div>
             </form>
         </div>
