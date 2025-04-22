83a1188e89f0c82cdfcbb87cdaec6317192b1624
John Neil <johnmontiano8@gmail.com>
2025-04-23 07:29:43 +0800

    Change input types for 'lot' and 'serial' fields from number to text for better flexibility

--- pages/filer_request.php
+++ pages/filer_request.php
@@ -112,12 +112,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
                     <div class="row mb-3">
                         <div class="col-md-3">
                             <label for="lot">Lot No. <span style="color: red;">*</span></label>
-                            <input type="number" name="lot" id="lot" class="form-control" required>
+                            <input type="text" name="lot" id="lot" class="form-control" required>
                         </div>
 
                         <div class="col-md-3">
                             <label for="serial">Serial No. <span style="color: red;">*</span></label><br>
-                            <input type="number" name="serial" id="serial" class="form-control" required>
+                            <input type="text" name="serial" id="serial" class="form-control" required>
                         </div>  
 
                         <div class="col-md-3">
