d3350ca51cc87fc0078b71840ebd59f4db6b390c
s3rb0g <raniegobres27@gmail.com>
2025-04-24 09:11:23 +0800

    Fix logout modal button character encoding for consistency

--- include/modal.php
+++ include/modal.php
@@ -1,11 +1,10 @@
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-  aria-hidden="true">
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header bg-danger text-white">
         <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
         <button class="close text-white" type="button" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">×</span>
+          <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
