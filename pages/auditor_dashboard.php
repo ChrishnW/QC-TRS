bb5b43fc26563f2253206ce3b95473233471b70d
s3rb0g <raniegobres27@gmail.com>
2025-04-25 14:45:33 +0800

    Remove commented-out card structure from auditor dashboard for cleaner code

--- pages/auditor_dashboard.php
+++ pages/auditor_dashboard.php
@@ -518,112 +518,11 @@
                         </div>
                     </div>
 
-                    <!-- <div class="card shadow mb-4 bg-light">
-                        <div class="col">
-                            <div class="row text-center">
-                                <div class="col-md-4">
-                                    <div class="card my-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b></b></span>
-                                    </div>
-                                </div>
-                               
-                                <div class="col-md-2">
-                                    <div class="card my-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>Findings</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card my-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>Remarks</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card my-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>Auditor</b></span>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-md-2">
-                                    <div class="card my-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>Date</b></span>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>Implementation Veification (as stated in the corrective action or after received the Root cause analysis report)</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>as</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>asd</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>asd</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>asd</b></span>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>Effective ness Verification (After 3 months)</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam, maxime! Accusamus id labore exercitationem recusandae provident ut expedita eveniet, facere sunt dignissimos impedit maxime soluta error modi quae ad laboriosam.</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>asd</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>asd</b></span>
-                                    </div>
-                                </div>
-
-                                <div class="col-md-2">
-                                    <div class="card text-center mb-2 d-flex align-items-center justify-content-center" style="min-height: 50px;">
-                                        <span class="text-center" style="font-size: 16px; word-wrap: break-word;"><b>asd</b></span>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>                     -->
-
                     <div class="card shadow mb-4 bg-light">
                         <div class="card m-2">
                             <div class="table-responsive">
                                 <table class="table" id="closed_dataTable" width="100%" cellspacing="0">
-                                    <thead class="">
+                                    <thead>
                                         <tr class="text-center">
                                             <th style="table-layout: fixed; width: 40%;"></th>
                                             <th style="table-layout: fixed; width: 20%;">Findings</th>
