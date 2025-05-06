1e0a6c7af7b1fb9f399e8dd8a28dfc27ef9eaa2d
John Neil <johnmontiano8@gmail.com>
2025-05-06 15:21:33 +0800

    Update email configuration and content in mail.php for Quality Control reports

--- pages/mail.php
+++ pages/mail.php
@@ -23,12 +23,29 @@ try {
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    $mail->setFrom('noreply@glory.com.ph', 'Glory Philippines Inc.'); //Sender's email and name
+
+    $mail->addAddress('requestor@glory.ph', 'Requestor'); //Add a recipient (Recipient's email and name)
+    $mail->addAddress('editor@glory.com', 'Editor'); 
+    $mail->addAddress('head@glory.com'); 
+    $mail->addAddress('factory@glory.com'); 
+    $mail->addAddress('supervisor@glory.com'); 
+    $mail->addAddress('coo@glory.com'); 
+
+    $mail->addCC('requestor@example.com');
+    $mail->addCC('editor@example.com');
+    $mail->addCC('head@example.com');
+    $mail->addCC('factory@example.com');
+    $mail->addCC('supervisor@example.com');
+    $mail->addCC('coo@example.com');
+
+    //Recipients
+    // $mail->setFrom('from@example.com', 'Mailer');
+    // $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    // $mail->addAddress('ellen@example.com');               //Name is optional
+    // $mail->addReplyTo('info@example.com', 'Information');
+    // $mail->addCC('cc@example.com');
+    // $mail->addBCC('bcc@example.com');
 
     //Attachments
     $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
@@ -36,9 +53,9 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'Quality Control Trouble Report';
+    $mail->Body    = 'Attached here is the <b>Quality Control Trouble Report</b> for your reference.';
+    // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
     echo 'Message has been sent';
