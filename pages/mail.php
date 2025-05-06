4bf67a847b0bd3173155d5593a23da9c248c0dc5
John Neil <johnmontiano8@gmail.com>
2025-05-07 07:44:23 +0800

    Comment out SMTP debug setting for clarity in mail.php. Message has been sent.

--- pages/mail.php
+++ pages/mail.php
@@ -13,7 +13,7 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                    //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.office365.com';                   //Set the SMTP server to send through
     //$mail->Host       = 'smtp-mail.outlook.com';                   
